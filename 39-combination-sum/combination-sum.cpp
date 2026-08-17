16a5e78ad212f550a7dcfccc7a2d9b98aa8097e6
Bibek-workspace <bibekagrahari2003@gmail.com>
2026-08-17 19:10:48 +0530

    Time: 2 ms (58.21%) | Memory: 14.4 MB (32.50%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> path;
+
+    void backtrack(vector<int>& candidates, int target, int start) {
+        if (target == 0) {
+            ans.push_back(path);
+            return;
+        }
+
+        for (int i = start; i < candidates.size(); i++) {
+            if (candidates[i] > target)
+                break;
+
+            path.push_back(candidates[i]);
+
+            backtrack(candidates, target - candidates[i], i);
+
+            path.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        backtrack(candidates, target, 0);
+        return ans;
+    }
+};
