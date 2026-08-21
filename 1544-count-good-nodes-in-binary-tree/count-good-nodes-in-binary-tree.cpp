6dac1576d51969386c230f36ced5275a51ce8ea9
Bibek-workspace <bibekagrahari2003@gmail.com>
2026-08-21 19:23:17 +0530

    Time: 106 ms (25.11%) | Memory: 88.4 MB (64.11%) - LeetSync

---
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dfs(TreeNode* node, int maxVal) {
+        if (node == NULL)
+            return 0;
+
+        int count = 0;
+
+        if (node->val >= maxVal) {
+            count = 1;
+        }
+
+        maxVal = max(maxVal, node->val);
+
+        count += dfs(node->left, maxVal);
+        count += dfs(node->right, maxVal);
+
+        return count;
+    }
+
+    int goodNodes(TreeNode* root) {
+        return dfs(root, root->val);
+    }
+};
